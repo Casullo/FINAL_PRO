1c01daf42ea829f1861fe9590668866e8ae0d7ed
Casullo <sebastiandanielcasullo@gmail.com>
2024-09-21 23:31:50 -0300

    Actualizando main

--- GameZoneFriends/apps/posts/models.py
+++ GameZoneFriends/apps/posts/models.py
@@ -1,8 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
-class Posts():
-    titulo=
-    contenido=
-    
+class Categoria(models.Model):
+    nombre = models.CharField()
+
+
+class Posts(models.Model):
+    titulo= models.CharField(max_length=250, null = False, blank= False, verbose_name="Titulo")
+    contenido= models.TextField(verbose_name="Contenido")
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
