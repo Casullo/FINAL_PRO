c08b2a3ce98520fcb83ad54aa303e738c3bda859
GabiPoko <gabipokorasky@gmail.com>
2024-09-25 11:12:31 -0300

    agregando Posts y categoría al admin

--- blog/apps/posts/admin.py
+++ blog/apps/posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Categoria, Posts
 
 # Register your models here.
+
+@admin.register (Posts)
+class PostsAdmin (admin.ModelAdmin):
+    listdisplay = ('Id', 'título', 'subtítulo', 'fecha', 'texto', 'activo', 
+                   'categoría', 'imagen', 'publicado' )
+    
+    admin.site.register (Categoria)
+
