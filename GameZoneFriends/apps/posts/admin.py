b70f90737323290f800841878055a73ced69c539
Casullo <sebastiandanielcasullo@gmail.com>
2024-10-03 20:34:24 -0300

    modificando pantalla admin y agrega filtro y busqueda

--- GameZoneFriends/apps/posts/admin.py
+++ GameZoneFriends/apps/posts/admin.py
@@ -6,7 +6,8 @@ from .models import Categoria, Post, User
 
 @admin.register(Post)
 class Postadmin(admin.ModelAdmin):
+    search_fields = ("titulo",)
     list_display = ("id", "titulo", "contenido", "fecha_publicacion", "autor", "categoria")
-
+    list_filter =["categoria", "autor__username"] # <---- hace referencia al campo username de la tabla post_user
 admin.site.register(Categoria)
 admin.site.register(User)
